3ad54d96d7a1ea6e483da345aaa2e5d08dcba167
RDP-3 <thilaknanu101@gmail.com>
2024-05-24 12:38:29 +0530

    backend setup router

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+require("dotenv").config()
+const cors = require("cors")
+const apiRouter = require("./routes")
+
+const PORT = 4000 || process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use("/api", apiRouter)
+
+
+app.get("/", (req, res) => {
+    res.send("server is working")
+})
+
+app.listen(PORT, () => {
+    console.log(`server was started on ${PORT} PORT`)
+})
