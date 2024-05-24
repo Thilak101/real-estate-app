8b1261f3afa6e3128d789c130f4634fef5b6e9d3
RDP-3 <thilaknanu101@gmail.com>
2024-05-24 10:07:21 +0530

    router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Home, Profie, Signin, Signup, About } from './pages'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profie />} />
+      <Route path='/Signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from "./home/Home"
+export {default as Profie} from "./profile/Profile"
+export {default as About} from "./about/About"
+export {default as Signin} from "./signin/Signin"
+export {default as Signup} from "./signup/Signup"
