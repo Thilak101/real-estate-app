f6946f24188ce94c9f798ce7e2165343d749182e
RDP-3 <thilaknanu101@gmail.com>
2024-05-24 10:35:30 +0530

    navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Home, Profie, Signin, Signup, About } from './pages'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React from "react";
+import { Home, Profie, Signin, Signup, About } from "./pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/profile' element={<Profie />} />
-      <Route path='/Signin' element={<Signin />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profie />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
