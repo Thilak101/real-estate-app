2b3e2fc6040fb2a71878108893884129da8ad49c
RDP-3 <thilaknanu101@gmail.com>
2024-05-27 15:10:03 +0530

    signup page complete

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,38 +1,90 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await fetch("/api/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate("/signin")
+      return
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+      return
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          name="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="username"
+          name="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="username"
+          name="password"
+          onChange={handleChange}
         />
-        <button 
-        className="bg-slate-700 
+        <button
+          className="bg-slate-700 
         text-white 
         p-3 
         rounded-lg 
         uppercase 
         hover:opacity-95
-        disabled:opacity-75">
-          Sign Up
+        disabled:opacity-75"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -41,6 +93,9 @@ const Signup = () => {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {
+        error && <p className="text-red-500 mt-5">{error}</p>
+      }
     </div>
   );
 };
--- server/routes/controllers/user.controllers.js
+++ server/routes/controllers/user.controllers.js
@@ -1,32 +1,33 @@
-const User = require("../../models/User")
-const errorHandler = require("../../middleware/error")
-const bcrypt = require("bcrypt")
+const User = require("../../models/User");
+const errorHandler = require("../../middleware/error");
+const bcrypt = require("bcrypt");
 
 const test = (req, res) => {
-    try {
-        res.json({msg: "user router"})
-    }
-    catch(err) {
-        res.json({msg: err.message})
-    }
-}
+  try {
+    res.json({ msg: "user router" });
+    return;
+  } catch (err) {
+    return res.json({ msg: err.message });
+  }
+};
 
-const signupController = async(req, res, next) => {
-    try {
-        const hassedPassword = bcrypt.hashSync(req.body.password, 8)
-        const user = await User.create({
-            username: req.body.username,
-            password: hassedPassword,
-            email: req.body.email
-        })
-        res.status(201).json({user, msg:"user created successfully", success: true})
-    }
-    catch(err) {
-        next(err)
-    }
-}
+const signupController = async (req, res, next) => {
+  try {
+    const hassedPassword = bcrypt.hashSync(req.body.password, 8);
+    const user = await User.create({
+      username: req.body.username,
+      password: hassedPassword,
+      email: req.body.email,
+    });
+    return res
+      .status(201)
+      .json({ user, msg: "user created successfully", success: true });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    test,
-    signupController
-}
+  test,
+  signupController,
+};
