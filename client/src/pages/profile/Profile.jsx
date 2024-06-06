188116272099737c5f3d969be7bf97edf690ee5c
RDP-3 <thilaknanu101@gmail.com>
2024-06-06 15:20:27 +0530

    uploading image done

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -116,7 +116,6 @@ const Profile = () => {
       dispatch(deleteUserFailure(data.message));
     }
   };
-  console.log(currentUser);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
