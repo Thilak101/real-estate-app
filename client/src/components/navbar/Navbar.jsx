24ba0260a8d6afc68bd55761b528e3f47b5f3e72
RDP-3 <thilaknanu101@gmail.com>
2024-07-04 18:23:13 +0530

    search tearm created

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const { currentUser } = useSelector((state) => state.user.user);
-
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if(searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl)
+    }
+  }, [location.search])
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -17,6 +31,7 @@ const Navbar = () => {
           </h1>
         </Link>
         <form
+          onSubmit={handleSubmit}
           action=""
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
@@ -24,8 +39,12 @@ const Navbar = () => {
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to={"/"}>
@@ -40,9 +59,10 @@ const Navbar = () => {
           </Link>
           <Link to={"profile"}>
             {currentUser ? (
-              <img src={currentUser.avatar} 
-              alt="profile"
-              className="rounded-full h-7 w-7 object-cover"
+              <img
+                src={currentUser.avatar}
+                alt="profile"
+                className="rounded-full h-7 w-7 object-cover"
               />
             ) : (
               <li className="text-slate-700 hover:underline">Sign in</li>
